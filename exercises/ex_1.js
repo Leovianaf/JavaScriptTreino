25ecc91a3a8c8ac77479a8fe30d3b1e325773cf5
genergia <sistema.web@genergia.com.br>
2021-07-07 12:01:02 -0300

    Exercises 1 e 2

---
+++ exercises/ex_1.js
@@ -0,0 +1,42 @@
+/* ### Transformar notas escolares
+
+Crie um algorítimo que transforma as notas do sistema numérico para sistemas de noatas em caracteres tipo A B C
+
+* de 90 pra cima - A
+* entre 80 - 89 - B
+* entre 70 - 79 - C
+* entre 60 - 69 - D
+* menor que 60 - F
+
+*/
+
+// let score = 93
+
+// function Calculator(score){
+//     let scoreA = score >= 90 && score<= 100
+//     let scoreB = score < 90 && score >= 80
+//     let scoreC = score < 80 && score >= 70
+//     let scoreD = score < 70 && score >= 60
+//     let scoreF = score < 60 && score >= 0
+
+//     if(scoreA){
+//         console.log("Parabéns, você tirou "+ score+ " é A!!!")
+//     } else if(scoreB){
+//         console.log("Parabéns, você tirou "+ score+ " é B!")
+//     } else if(scoreC){
+//         console.log("Bom, você tirou "+ score+ " é C!")
+//     } else if(scoreD){
+//         console.log("Da pra melhorar, você tirou "+ score+ " é D!")
+//     } else if(scoreF){
+//         console.log("infelizmente você vai pra recuperação, tirou "+ score+ " é F!")
+//     } else{
+//         console.log("Nota inválida")
+//     }
+// }
+
+// Calculator(100)
+// Calculator(80)
+// Calculator(70)
+// Calculator(60)
+// Calculator(0)
+// Calculator(-1)
